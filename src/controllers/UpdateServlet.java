7b274243fbc6b97b760d75a755dc3020ddb2ec07
ayano.homma <annin0408@gmail.com>
2023-02-15 21:11:08 +0900

    バリデーションを追加

--- src/controllers/UpdateServlet.java
+++ src/controllers/UpdateServlet.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.io.IOException;
 import java.sql.Timestamp;
+import java.util.List;
 
 import javax.persistence.EntityManager;
 import javax.servlet.RequestDispatcher;
@@ -11,7 +12,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import antlr.collections.List;
 import models.Message;
 import models.validators.MessageValidator;
 import utils.DBUtil;
@@ -36,60 +36,61 @@ public class UpdateServlet extends HttpServlet {
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String _token = request.getParameter("_token");
         if(_token != null && _token.equals(request.getSession().getId())) {
-                EntityManager em = DBUtil.createEntityManager();
+            EntityManager em = DBUtil.createEntityManager();
 
-                // セッションスコープからメッセージのIDを取得して
-                // 該当のIDのメッセージ1件のみをデータベースから取得
-                Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
+            // セッションスコープからメッセージのIDを取得して
+            // 該当のIDのメッセージ1件のみをデータベースから取得
+            Message m = em.find(Message.class, (Integer)(request.getSession().getAttribute("message_id")));
 
-                // フォームの内容を各フィールドに上書き
-                String title = request.getParameter("title");
-                m.setTitle(title);
+            // フォームの内容を各フィールドに上書き
+            String title = request.getParameter("title");
+            m.setTitle(title);
 
-                String content = request.getParameter("content");
-                m.setContent(content);
+            String content = request.getParameter("content");
+            m.setContent(content);
 
-                Timestamp currentTime = new Timestamp(System.currentTimeMillis());
-                m.setUpdated_at(currentTime);       // 更新日時のみ上書き
+            Timestamp currentTime = new Timestamp(System.currentTimeMillis());
+            m.setUpdated_at(currentTime);       // 更新日時のみ上書き
 
-                // バリデーションを実行してエラーがあったら編集画面のフォームに戻る
-                List<String> errors = MessageValidator.validate(m);
-                if(errors.size() > 0) {
-                    em.close();
+            // バリデーションを実行してエラーがあったら編集画面のフォームに戻る
+            List<String> errors = MessageValidator.validate(m);
+            if(errors.size() > 0) {
+                em.close();
 
-                    // フォームに初期値を設定、さらにエラーメッセージを送る
-                    request.setAttribute("_token", request.getSession().getId());
-                    request.setAttribute("message", m);
-                    request.setAttribute("errors", errors);
+                // フォームに初期値を設定、さらにエラーメッセージを送る
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("message", m);
+                request.setAttribute("errors", errors);
 
-                    RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
-                    rd.forward(request, response);
-                } else {
-                    // データベースを更新
-                    em.getTransaction().begin();
-                    em.getTransaction().commit();
-                    request.getSession().setAttribute("flush", "更新が完了しました。");
-                    em.close();
-
-                    // セッションスコープ上の不要になったデータを削除
-                    request.getSession().removeAttribute("message_id");
-
-                    // indexページへリダイレクト
-                    response.sendRedirect(request.getContextPath() + "/index");
-                }
-
-             // データベースを更新
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/edit.jsp");
+                rd.forward(request, response);
+            } else {
+                // データベースを更新
                 em.getTransaction().begin();
                 em.getTransaction().commit();
-                request.getSession().setAttribute("flush", "更新が完了しました。");       // ここを追記
+                request.getSession().setAttribute("flush", "更新が完了しました。");
                 em.close();
 
                 // セッションスコープ上の不要になったデータを削除
                 request.getSession().removeAttribute("message_id");
 
-                // indexページリダイレクト
+                // indexページへリダイレクト
                 response.sendRedirect(request.getContextPath() + "/index");
+            }
+
+
+            // データベースを更新
+            em.getTransaction().begin();
+            em.getTransaction().commit();
+            request.getSession().setAttribute("flush", "更新が完了しました。");       // ここを追記
+            em.close();
+
+            // セッションスコープ上の不要になったデータを削除
+            request.getSession().removeAttribute("message_id");
+
+            // indexページへリダイレクト
+            response.sendRedirect(request.getContextPath() + "/index");
         }
     }
-
 }
+
